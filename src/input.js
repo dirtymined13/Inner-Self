76c144a6faaf153f95ee25f0642ef41fa7ae0db1
LewdLeah <daisy.anais.197@gmail.com>
2026-01-02 21:09:19 -0500

    Create input.js

---
+++ src/input.js
@@ -0,0 +1,7 @@
+// Your "Input" tab should look like this
+InnerSelf("input");
+const modifier = (text) => {
+  // Any other input modifier scripts can go here
+  return { text };
+};
+modifier(text);
